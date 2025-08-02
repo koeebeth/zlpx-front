b74bdc3c6c87e7bfeff3c2a64fa5645bb5df612f
koeebeth <96339397+koeebeth@users.noreply.github.com>
2025-08-03 01:42:45 +0300

    add retrieving data from tg webapp

--- src/pages/Main/Profile/Profile.tsx
+++ src/pages/Main/Profile/Profile.tsx
@@ -2,6 +2,9 @@ import { Avatar } from "@mui/material";
 import { FC } from "react";
 
 export const Profile: FC = () => {
+  // @ts-expect-error telegram window api
+  const user = window.Telegram?.WebApp?.initDataUnsafe?.user;
+
   return (
     <>
       <div className="w-full p-4 text-center text-xl font-medium text-light-purple">
@@ -9,13 +12,18 @@ export const Profile: FC = () => {
       </div>
       <div className="flex flex-col p-2 bg-zinc-300 flex-1 dark:bg-zinc-900">
         <div className="rounded-lg bg-zinc-400 dark:bg-zinc-800 h-28 flex items-center justify-between p-2 px-6">
-        <div>
-            <h4 className="mb-2 text-sm dark:text-zinc-400">туту</h4>
-            <h3 className="font-semibold mb-2 dark:text-zinc-200">Тутуту Тутуту</h3>
-            <h4 className="text-x-purple text-sm dark:text-light-purple">тутуту</h4>
-        </div>
-          <Avatar
-          sx={{width: 70, height: 70}}>ТУ</Avatar>
+          <div>
+            <h4 className="mb-2 text-sm dark:text-zinc-400">
+              {`@${user ? user.username : "username"}`}
+            </h4>
+            <h3 className="font-semibold mb-2 dark:text-zinc-200">
+              {user ? `${user.first_name} ${user.last_name}` : "Имя Фамилия"}
+            </h3>
+            <h4 className="text-x-purple text-sm dark:text-light-purple">
+              тутуту
+            </h4>
+          </div>
+          <Avatar sx={{ width: 70, height: 70 }}>ТУ</Avatar>
         </div>
       </div>
     </>
